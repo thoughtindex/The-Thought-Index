d3b09806aa7f937916dd1630fe8a051832b12c86
thoughtindex <khadatkararjun066@gmail.com>
2026-06-12 12:16:12 +0000

    Co-authored-by: 22H - Hordiychuk - Artemis <22H-HORDIYCHUK@beths.bexley.sch.uk>

--- menu_parser.js
+++ menu_parser.js
@@ -1,18 +1,7 @@
-const menu = require("./menu.json")
+
 
 function ParseMenu(file) {
-    // const BaseContainer = document.getElementById("BaseContainer")
-    // Object.entries(menu).forEach((entry) => {
-    //     const [Category, Specifics] = entry;
-    //     Object.entries(Specifics).forEach((second) => {
-    //         const [Specific, Data] = second;
-    //         console.log(`${Specific}, ${Data.Ingredients}, ${Data.Price}, ${Data.Image}`)
-    //         let SpecificNode = BaseContainer.cloneNode()
-    //         for(const Node of SpecificNode.childNodes) {
-    //             console.log(Node.nodeName)
-    //         }
-    //     })
-    // });
+
 }
 
 module.exports = {
